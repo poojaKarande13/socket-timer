798e5a8fbed48d4ce769e59011d7e6d38316064f
= <root@Poojas-MacBook-Pro.local>
2018-11-26 16:42:01 -0600

    Socket with React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { subscribeToTimer } from './api';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    subscribeToTimer((err, timestamp) => this.setState({
+      timestamp
+    }));
+  }
+
+  state = {
+    timestamp: 'no timestamp yet'
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p className="App-intro">
+            This is the timer value: {this.state.timestamp}
           </p>
           <a
             className="App-link"
